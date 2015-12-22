5b77ae1a7b3c8feb934257718686c7242391b727
Ryan Ostrom <ryan.ostrom@praxent.com>
2015-12-22 14:27:55 -0600

    remove xcode install and validate git exists
    Signed-off-by: Ryan Ostrom <ryan.ostrom@praxent.com>

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -13,12 +13,6 @@ function display() {
     echo "-----> $1"
 }
 
-display "Installing xcode with git"
-touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
-PROD=$(softwareupdate -l | grep "\*.*Command Line" | head -n 1 | awk -F"*" '{print $2}' | sed -e 's/^ *//' | tr -d '\n');
-softwareupdate -i "$PROD" -v;
-echo -ne '\n'; 
-
 if [ ! `which pip` ]
 then
     display "Installing pip"
